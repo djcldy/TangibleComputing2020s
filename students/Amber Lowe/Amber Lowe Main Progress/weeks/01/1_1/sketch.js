5df39771996d21954134921c6611e4b69eafc05d
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-02-12 00:54:23 +0400

    Wednesday Final Update

--- students/Amber Lowe/Amber Lowe Main Progress/weeks/01/1_1/sketch.js
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/01/1_1/sketch.js
@@ -51,8 +51,8 @@
                   let b = random(1,2000)
                   ellipse(a,b,10,10)
                   ellipse(width-a,b,a/10,a/10)
-                  //line(a,b,0,0)
-                  //triangle(a,b,10,10)
+                  line(a,b,0,0)
+                  triangle(a,b,10,10)
                   rect(a,b,10,10)//r++
                   //point(a,b,10,10)
                   //quadrilateral(a,b,10,10,)
