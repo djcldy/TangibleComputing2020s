0ade12f124dc38a46569af1643c3f8b4484c7ab5
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-19 19:04:46 +0400

    f

--- students/sana/weeks/03/Day03/sketch.js
+++ students/sana/weeks/03/Day03/sketch.js
@@ -1,54 +1,39 @@
-/unction setup(){
 
-  createCanvas(800,800);
-  background(255)
+let clockFont;
 
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  clockFont = loadFont("digital-7.ttf");
 }
 
-
-function draw(){
-
-  drawIceCream(second(),200,1)
-  drawIceCream(minute(),400,2)
-  drawIceCream(hour(),600,3)
-
-  if (second()>58){
-    background(255)
-  }
-
+function windowResized()
+{
+ resizeCanvas(windowWidth, windowHeight);
 }
 
-function drawIceCream(time,posX,radius){
+function draw() {
+  background(0);
+  
 
-  let x = posX + sin(time)*20
-  let y = height- time/60*height + random(-3,3)
-  let alpha = time/60*255
-  fill(alpha,0,255-alpha,alpha)
-  crazyHeart(x,y,time*radius,time*radius)
-
+clock();
 }
 
-function heart(x,y,radius,radius){
-
-  noStroke()
-  ellipse(x-radius/4,y,radius/2)
-  ellipse(x+radius/4,y,radius/2)
-  triangle(x-radius/2,y, x+radius/2,y, x, y+radius/2);
-
-}
-
-
-function crazyHeart(x,y,radius,radius){
-
-  push()
-  translate(x,y)
-
-  rotate(random())
-  // noStroke()
-  ellipse(radius/4,0,radius/2)
-  ellipse(radius/4,0,radius/2)
-  triangle(radius/2,0, radius/2,y,0,radius/2);
-  pop()
+function clock()
+{
+ fill("red");
+  textFont(clockFont);
+  textAlign(CENTER, CENTER);
+  textSize(width/4.25);
+   var gif_createImg;
+  let Hour = hour();
+  let min = minute();
+  let secs = second()
+  let noon = Hour >= 12? " PM" : " AM"
+  if(min < 10)
+    min = "0"+min
+  Hour%=12
+  text(Hour+":"+min+":"+secs+noon, width/2, height/2); 
+  background:("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif”);
 
 }
