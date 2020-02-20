b802ac4e0b8ecfd58214a830c01d7f8d26b8f690
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-20 16:23:45 +0400

    update

--- students/sana/weeks/03/Day03/sketch.js
+++ students/sana/weeks/03/Day03/sketch.js
@@ -1,16 +1,24 @@
 
+var gif_loadImg;
+
+
+
 function setup() {
   createCanvas(800, 800);
-
-  
+  gif_loadImg = loadImage("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
+ 
 }
 
 function draw() {
   background(0);
- 
-
   let sc = second();
-var gif_createImg;
+  if (sc>15){
+  	background(gif_loadImg);
+  }else{
+
+  	background(255)
+  }
+
 
  strokeWeight(8);
   
@@ -19,9 +27,8 @@ var gif_createImg;
   let end = map(sc, 0, 60, 0, 360);
   arc(400, 200, 300, 300, 0, end,PIE);
   
-  	if (second()>58){
-      gif_createImg = createImg("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
-   image(gif_createImg, 50, 50);
+  
+      
+}
 
-}
-}
+
