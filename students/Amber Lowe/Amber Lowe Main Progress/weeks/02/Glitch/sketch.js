10ff27f6c6c364eb23819de1cce43a7212acd54f
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-02-20 17:55:38 +0400

    Update to week 2 "Glitch"

---
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/02/Glitch/sketch.js
@@ -0,0 +1,76 @@
+/*
+
+    Example Code: Interactive Gradient Array
+
+*/
+
+
+function setup(){
+
+  createCanvas(1200,600);
+  stroke(255)
+
+}
+
+/*function draw(){
+
+	let x = width/2 + random(-3,3)
+	let y = height- second()/60*height
+	let alpha = second()/60*255
+
+	fill(alpha,0,255-alpha,alpha)
+	noStroke()
+
+	ellipse(x,y,second(),second())
+
+	if(second()>58){
+		background(255)
+	}
+
+ 
+   
+
+}*/
+
+function draw(){
+
+	drawIceCream(second(),200,1)
+	drawIceCream(minute(),400,2)
+	drawIceCream(hour(),600,3)
+
+	if (second()>58){
+		background(255)
+	}
+}
+function drawIceCream(time,posX,radius){
+
+	let x = posX + sin(time)*20
+	let y = height- time/60*height + random(-3,3)
+	let alpha = time/60*255
+
+	fill(alpha,255,0-alpha,alpha)
+	noStroke()
+
+	//ellipse(x,y,time*radius,time*radius)
+	heart(x,y,time*radius,time*radius)
+
+
+}
+
+function heart(x,y,radius,radius){
+
+  background(0)
+  push()
+  translate(x,y)
+  rotate(x/100)
+  scale(sin(y/100)+1)
+
+
+    ellipse(x-radius/2,y,radius/2)
+	ellipse(x+radius/4,y,radius/2)
+	arc(x-radius/2,y,x+radius/2,y,x, y+radius/2);
+	pop()
+}
+
+
+
