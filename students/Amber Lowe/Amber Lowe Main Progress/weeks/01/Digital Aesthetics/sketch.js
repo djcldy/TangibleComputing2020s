b1c602170617e49abe6ec3e717054e154f66c3b6
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-02-20 11:55:55 +0400

    Update

---
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/01/Digital Aesthetics/sketch.js
@@ -0,0 +1,63 @@
+// TenThousandX Assignment: 
+          // Add to this code to create a drawing 
+          // Note: do not delete anything
+
+          function setup(){
+
+             // do not change anything in setup 
+
+              createCanvas(2000,600)
+              TenThousandX()
+
+          }
+
+          function TenThousandX(){
+
+             //add code here 
+             //background(34,122,200)
+
+             //fill(255,0,0)
+             //stroke(10)
+             //strokeWeight(5)
+
+             //ellipse(100,80,100,100)
+
+             //fill(255)
+             
+
+             //ellipse(200,200,100,100)
+             //noStroke()
+             //stroke(40,255,8)
+             //noFill()
+             //stroke(0,100)
+             //stroke(255,255,255)
+             //fill(0)
+
+             //strokeWeight(7,4,3)
+            
+
+            
+
+             //for (var random = 0;  random++)
+
+             //fill (var random = 0;  random++}
+
+
+
+              //for ( var x = 0; x < 10000; x++)
+
+              	var img;
+
+
+function setup() {
+  createCanvas(500, 500);
+  img = createImg('http://www.petwebsite.com/hamsters/hamsters_images/syrian-hamster_000008437184.jpg');
+  img.hide();
+  background(100);
+
+}
+
+function draw() {
+  image(img, 0,0,width,height); 
+  
+}
