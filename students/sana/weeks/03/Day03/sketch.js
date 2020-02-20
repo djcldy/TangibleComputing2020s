7f8f4b4561a1fa4f23543f2ad744e54c0a9f1d78
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-02-20 16:15:04 +0400

    update

--- students/sana/weeks/03/Day03/sketch.js
+++ students/sana/weeks/03/Day03/sketch.js
@@ -1,28 +1,36 @@
+var gif_loadImg
 
 function setup() {
-  createCanvas(800, 800);
 
+  createCanvas(800, 800);
+    gif_loadImg = loadImage("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
+  // gif_createImg = createImg("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
   
 }
 
+
 function draw() {
-  background(0);
- 
 
-  let sc = second();
-var gif_loadImg,gif_createImg;
+  // background(0);
 
- strokeWeight(8);
+  let sc = second()
+
+  if (sc > 15){
+
+    background(gif_loadImg);
+  
+  } else { 
+
+    background(255)
+
+  }
+
+  strokeWeight(8);
   
   stroke(55, 125, 100);
   fill(145,47,90,100);
   let end = map(sc, 0, 60, 0, 360);
   arc(400, 200, 300, 300, 0, end,PIE);
   
-  	if (second()>58){
-    gif_loadImg = loadImage("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
-  gif_createImg = createImg("https://media.giphy.com/media/3ohfFIJ9M9XCUkE6Na/giphy.gif");
-   image(gif_loadImg, 50, 50);
 
-}
 }
