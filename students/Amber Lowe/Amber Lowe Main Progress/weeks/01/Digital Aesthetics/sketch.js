054cb10dd601a087d888dcd68114e5627b4bbdd4
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-02-20 12:42:23 +0400

    P5 editor update

--- students/Amber Lowe/Amber Lowe Main Progress/weeks/01/Digital Aesthetics/sketch.js
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/01/Digital Aesthetics/sketch.js
@@ -1,24 +1,63 @@
-var img;
+for ( var x = 0; x < 10000; x++)
+var x = 0
+var y = 0
 
-function preload() {
-  // img = loadImage('hamster.jpg');
-  // img = loadImage('http://www.petwebsite.com/hamsters/hamsters_images/syrian-hamster_000008437184.jpg');
-}
 
 function setup() {
-  createCanvas(1000, 1000);
-  img = createImg('https://i.pinimg.com/originals/05/59/3e/05593e925e651314820bbb215b223a5f.jpg');
-  img.hide();
+  createCanvas(500, 500);
+  img = createImg('https://i.pinimg.com/originals/db/72/94/db729453eab32cba9463313eaa64921e.jpg');
+ 
+  // img.hide();
   background(100);
-  // loadImage('hamster.jpg', imageLoaded);
-  // image(img, 0, 0);
+
 }
 
 function draw() {
-  image(img, 0,0,width,height); 
+  background(255)
+  push()
+  translate(x,y)
+  rotate(x/100)
+  scale(sin(y/100)+1)
+  
+  image(img,0,0,100,100);
+  
+  pop()
+  x+= random()
+  y+= random()
+  if (x > width) x = 0
+  if (y >height) y = 0
+
   
 }
 
-// function imageLoaded(img) {
-//   image(img, 0, 0);
-// }
+var img;
+var x = 10
+var y = 0
+
+
+function setup() {
+  createCanvas(500, 500);
+  img = createImg('https://images.unsplash.com/photo-1533003505519-6a9b92ed4911?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80');
+ 
+  // img.hide();
+  background(100);
+
+}
+
+function draw() {
+  background(0)
+  push()
+  translate(x,y)
+  rotate(x/100)
+  scale(sin(y/100)+1)
+  
+  image(img,0,0,100,100);
+  
+  pop()
+  x+= (10,3)
+  y+= random()
+  if (x > width) x = 7
+  if (y >height) y = 0
+
+  
+}
