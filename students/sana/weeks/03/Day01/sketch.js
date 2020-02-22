a4def539d06e6808fb1f1d727b98bd4d8ba7c943
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-22 21:11:52 +0400

    Update sketch.js

--- students/sana/weeks/03/Day01/sketch.js
+++ students/sana/weeks/03/Day01/sketch.js
@@ -22,7 +22,7 @@ function draw(){
   drawIceCream(hour(),600,3)
 
   if (second()>58){
-    background(255)
+    background(65)
   }
 
 }
@@ -30,22 +30,12 @@ function draw(){
 function drawIceCream(time,posX,radius){
 
   let x = posX + sin(time)*20
-  let y = height- time/60*height + random(-3,3)
+  let y = height+time/60*height + rotate(-90)
   let alpha = time/60*255
   fill(alpha,0,255-alpha,alpha)
   crazyHeart(x,y,time*radius,time*radius)
 
 }
-
-function heart(x,y,radius,radius){
-
-  noStroke()
-  ellipse(x-radius/4,y,radius/2)
-  ellipse(x+radius/4,y,radius/2)
-  triangle(x-radius/2,y, x+radius/2,y, x, y+radius/2);
-
-}
-
 
 function crazyHeart(x,y,radius,radius){
 
@@ -53,10 +43,10 @@ function crazyHeart(x,y,radius,radius){
   translate(x,y)
 
   rotate(random())
-  // noStroke()
-  ellipse(radius/4,0,radius/2)
-  ellipse(radius/4,0,radius/2)
-  triangle(radius/2,0, radius/2,y,0,radius/2);
+   noStroke()
+  rect(radius/4,0,radius/2,10,y+50)
+ rect(radius/2,0,radius/4,12,y+2)
+ triangle(radius/4,0, radius/4,x,y-10);
   pop()
 
 }
