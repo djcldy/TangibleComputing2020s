1311c19dde0b841779a41d289a106215f9142e18
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-22 22:02:50 +0400

    update

--- students/sana/weeks/03/Day07/sketch.js
+++ students/sana/weeks/03/Day07/sketch.js
@@ -1,58 +1,35 @@
+var time = 0;
 
-    let bg
-  function setup() {
-    createCanvas(800, 800);
+function setup() { 
+  createCanvas(700, 700);
+  frameRate(1000);
+} 
+
+function rainbowColor() {
     
- bg = loadImage('file:///Users/sanamohamed/Documents/GitHub/TangibleComputing2020s/students/sana/weeks/03/Day06/mario_PNG126.png');
+  let Hr = hour();
+  let Mn= minute();
+  let Sc = second();
+  let time = h*3600 + m*60 + s;
+
+ 
 }
-function draw() {
-  
-background (bg);
-  let Hr = hour ()
-  let Mn = minute()
-  let Sc = second ()
-  let alpha = time/60*255
-  fill(alpha,0,255-alpha,alpha)
 
-  strokeWeight (6)
-  stroke (100,55,119)
-  fill(17,144,230)
-   rect(width/7, Sc*5, height/2, Hr, 10, Mn) 
-  push()
-  translate (100,100)
-  
-  strokeWeight (5)
-  stroke (10,45,19)
-  fill (34, 44, 150)
-  rect (width/6, Sc*5, height/3, Hr, 10, Mn) 
-  push ()
-  translate (100, 100)
-  
-  strokeWeight (4)
-  stroke (190,5,11)
-  fill (99, 176, 177)
-  rect (width/5, Sc*5, height/4, Hr, 10, Mn) 
-  push ()
-  translate (100, 100)
-  
-  strokeWeight (3)
-  stroke (44,55,99)
-  fill (66, 10, 44)
-  rect(width/4, Sc*5, height/5, Hr, 10, Mn)
-  push ()
-  translate (100, 100)
-  
-  strokeWeight (2)
-  stroke (50,75,89)
-  fill (90, 160, 80)
-  rect(width/3, Sc*5, height/6, Hr, 10, Mn)
-  push ()
-  translate (100, 100)
 
-  strokeWeight (1)
-  stroke (210,55,9)
-  fill (254, 112, 210)
-  rect (width/2, Sc*5, height/7, Hr, 10, Mn)
-  pop ()
+function draw() { 
+noFill();
+  let Sc = second();
+ 
+    let alpha = time/60*255
+   fill(alpha,0,255-alpha)
+  stroke(alpha,100,alpha-101,14);
+  
+  let myRadius = map(Sc,0,60,0,500);
 
-}
+  ellipse(350, 350, myRadius);
+
+ 
+
+  time = time + 4;
+}  
+  
