d7feb8d87c97c8194149a9809c5e22748ac5621f
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-22 22:32:28 +0400

    update

--- students/sana/weeks/03/Day07/sketch.js
+++ students/sana/weeks/03/Day07/sketch.js
@@ -1,35 +1,45 @@
-var time = 0;
 
+var time = 0;
 function setup() { 
   createCanvas(700, 700);
   frameRate(1000);
+
 } 
+
 
 function rainbowColor() {
     
   let Hr = hour();
   let Mn= minute();
   let Sc = second();
-  let time = h*3600 + m*60 + s;
-
- 
+   
 }
 
-
 function draw() { 
-noFill();
+
+ push()
+ translate(100,100)
+  let alpha = time/60*255
   let Sc = second();
+     stroke(90,50,154,10)
+     fill(alpha,0,255-alpha,alpha)
+  let radius = map(Sc,0,60,0,500)
+ circle(400, 400, radius);
+strokeWeight(4);
+     stroke(66,5,55,100)
+     fill(alpha,0,255-alpha,alpha)
+ circle(300,300, radius);
+strokeWeight(2);
+     stroke(40,75,15,100)
+     fill(alpha,0,255-alpha,alpha)
+ circle(200,200, radius);
+strokeWeight(1);
+     stroke(80,5,45,140)
+     fill(alpha,0,255-alpha,alpha)
+ circle(100,100, radius);
+pop()
  
-    let alpha = time/60*255
-   fill(alpha,0,255-alpha)
-  stroke(alpha,100,alpha-101,14);
-  
-  let myRadius = map(Sc,0,60,0,500);
 
-  ellipse(350, 350, myRadius);
-
- 
-
-  time = time + 4;
 }  
+
   
