06adc6f8d911dbfa34530ea41fab7084b87eeb3f
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-22 21:24:11 +0400

    update

--- students/sana/weeks/03/Day07/sketch.js
+++ students/sana/weeks/03/Day07/sketch.js
@@ -1,65 +1,58 @@
-/*
 
-    Example Code: Interactive Gradient Array
+    let bg
+  function setup() {
+    createCanvas(800, 800);
+    
+ bg = loadImage('file:///Users/sanamohamed/Documents/GitHub/TangibleComputing2020s/students/sana/weeks/03/Day06/mario_PNG126.png');
+}
+function draw() {
+  
+background (bg);
+  let Hr = hour ()
+  let Mn = minute()
+  let Sc = second ()
+  let alpha = time/60*255
+  fill(alpha,0,255-alpha,alpha)
 
-*/
+  strokeWeight (6)
+  stroke (100,55,119)
+  fill(17,144,230)
+   rect(width/7, Sc*5, height/2, Hr, 10, Mn) 
+  push()
+  translate (100,100)
+  
+  strokeWeight (5)
+  stroke (10,45,19)
+  fill (34, 44, 150)
+  rect (width/6, Sc*5, height/3, Hr, 10, Mn) 
+  push ()
+  translate (100, 100)
+  
+  strokeWeight (4)
+  stroke (190,5,11)
+  fill (99, 176, 177)
+  rect (width/5, Sc*5, height/4, Hr, 10, Mn) 
+  push ()
+  translate (100, 100)
+  
+  strokeWeight (3)
+  stroke (44,55,99)
+  fill (66, 10, 44)
+  rect(width/4, Sc*5, height/5, Hr, 10, Mn)
+  push ()
+  translate (100, 100)
+  
+  strokeWeight (2)
+  stroke (50,75,89)
+  fill (90, 160, 80)
+  rect(width/3, Sc*5, height/6, Hr, 10, Mn)
+  push ()
+  translate (100, 100)
 
+  strokeWeight (1)
+  stroke (210,55,9)
+  fill (254, 112, 210)
+  rect (width/2, Sc*5, height/7, Hr, 10, Mn)
+  pop ()
 
-
-
-        function setup(){
-
-             // do not change anything in setup 
-
-              createCanvas(1000,1000)
-              TenThousandX()
-
-          }
-
-          function TenThousandX(){
-//background(90,155,215)
-//fill(255,0,0)
-//stroke(100)
-
-//ellipse(50,50,50,100)
-//fill(255,0,0)
-//strokeWeight(5)
-//ellipse(200,50,200,100)
-//fill(255,0,0)
-//strokeWeight(30)
-
-              
-              for ( var x = 0; x < 10000; x++ ){
-                //.......... it will be in same position 10000 unless we put random
-
-                //console.log(x)
-                //let a = random(width)
-                //let b = random(height)
-
-                //if we want them all vertical let b= x/10000*height
-                let a = sin(x/100)*width/2
-                let b = x/10000*height
-
-              ellipse(a,b,a/60,a/60)
-              ellipse(width-a,b,a/6,a/10)
-              stroke(200,60,25,20)
-                ellipse(a,b,a/50,a/50)
-              ellipse(width-a,b,a/150,a/10)
-              stroke(10,60,90,60)
-              ellipse(a,b,a/40,a/40)
-              ellipse(width-a,b,a/10,a/10)
-              stroke(10,60,0,2)
-              ellipse(a,b,a/40,a/40)
-              ellipse(width-a,b,a/10,a/10)
-              stroke(70,80,80,6)
-            strokeWeight(4)
-
-              
-//if we want lines we put line(0,b,a,b), 0 to start the line from begining of canvas
-                  // add code here 
-
-
-
-              }
-
-            }
+}
