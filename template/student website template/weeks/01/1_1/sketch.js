aeffa7fa4f9076f8b826bf67eeca459de91d1d40
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-23 16:18:25 +0400

    updte

--- template/student website template/weeks/01/1_1/sketch.js
+++ template/student website template/weeks/01/1_1/sketch.js
@@ -1,38 +1,53 @@
-/*
-
-    Example Code: Interactive Gradient Array
-
-*/
-
-
-function setup(){
-
-  createCanvas(800,600);
-  stroke(255)
-
+let capture 
+          function setup(){
+              createCanvas(800,800)
+              capture=createCapture(VIDEO)
+              capture*hide()
+            
 }
 
-function draw(){
-  background(255)
+ function draw(){
+background(255)
 
-  let numCol = 20
-  let numRow = 20
-  let stepX = width / numCol // height of box 
-  let stepY = height / numRow // width of box  
+              //eye(50,50,[34,155,20],40)
+              //eye(100,100,[154,15,70],20)
+              //eye(200,25,[78,95,20],10)
+let spacing = (50)
+for (var x =0; x<width; x+= spacing){
 
-  for (var col = 0; col < numRow; col++){
+  for (var y =0; y<height; y+= spacing/2){
+    //if (randoum()>0.9)continue
 
-    for (var row = 0; row < numCol; row++){
-    
-      fill(col/numCol*255,row/numRow*255,mouseX/width*255)   
-      push()
-      translate(row*stepX,col*stepY)
-      rotate(dist(row*stepX,col*stepY,mouseX,mouseY)/600*TWO_PI) 
-      rect(0,0,stepX,stepY) 
-      pop()
+    let color =capture.get(x,y)
+    fill(color
+     rect(x,y,spacing,spacing/2)
 
-    }
+  //eye(x,y,color,spacing/4)
 
-  }
+          }
+        }
+      }
 
-}
+  function eye(x,y,color,r1){
+
+
+
+
+//let y =50
+//let r1=10
+//let r2=10
+fill(255)
+ ellipse(x,y,r1*4,r1*2)// white
+fill(color)
+ ellipse(x,y,r1*2,r1*2)// iris 
+ fill(20)
+ ellipse(x,y,r1,r1)//pupil
+ fill(224)
+ ellipse(x+r1/2,y-r1/2,r1/2,r1/2)//pupil
+
+
+ 
+
+              }
+
+            
