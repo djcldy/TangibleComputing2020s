d5073e064a5d5fa267e9e7f001ea8a32722a2445
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-28 15:53:13 +0400

    Update sketch.js

--- students/sana/weeks/04/Day02/sketch.js
+++ students/sana/weeks/04/Day02/sketch.js
@@ -1,59 +1,68 @@
-/*
-
-Gradient Array
-
-*/
-
 
 
 function setup() {
   createCanvas(800, 800);
-  background(55);
-
-}
-function draw(time) {
-  strokeWeight(2);
-  translate(width/2, height/2);
-  if (second()>58)
-  background(55);
-  
-  var radius = int(min(width, height) / 2);
-  var numPoints = 60;
-  var angle = TWO_PI/numPoints;
-  
-  
-  var secondsRadius = radius * 0.72;
-  var minutesRadius = radius * 0.60;
-  var hoursRadius = radius * 0.50;
-  var clockDiameter = radius * 1.8;
-
-  
-
-  var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
-  var m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
-  var h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
-  var alpha = time/60*255
+  background(255);
  
 
-  strokeWeight(80);
-  stroke(50,125,170);
-  
-  beginShape(POINTS);
-  var i = 0;
-  while (i < numPoints ) {
-      x = cos(angle*i) * secondsRadius; 
-      y = sin(angle*i) * secondsRadius;
-      vertex(x, y);
-      i++;
-  }
-  endShape(); 
  
-  strokeWeight(2);
-  line(0, 0, cos(s) * secondsRadius, sin(s) * secondsRadius);
-  strokeWeight(3);
-  line(0, 0, cos(m) * minutesRadius, sin(m) * minutesRadius);
-  strokeWeight(5);  
-  line(0, 0, cos(h) * hoursRadius, sin(h) * hoursRadius);
-  
-  
+ let spacing = 300
+for (var x =0; x<width; x+= spacing){
+
+ for (var y =0; y<height; y+= spacing){
+//      //cat(x,y,[random(255),random(255),random(255)],random(2,12))
+     cat(x,y,[random(255)],10,2)
 }
+
+function cat(x,y,color,r1) {
+
+ 
+
+
+// fill(145,20,129)
+//  ellipse(x+r1*2,y,r1*6,r1*4)
+//  fill(69,112,55)
+//  ellipse(x-r1*2,y,r1*6,r1*4)
+//  fill(80,113,241)
+//  ellipse(x,y-r1*2,r1*6,r1*3,r1*4)
+//  fill(187,33,166)
+//  ellipse(x,y+r1*2,r1*6,r1*4,r1*5)
+//  fill(85,40,33)
+//  ellipse(x,y,r1*4,r1*6,r1*2)
+//   fill(85,17,220)
+//   ellipse(x+r1*2,y,r1*4,r1*2)
+//   fill(162,147,150)
+//   ellipse(x-r1*2,y,r1*4,r1*2)
+ fill(255)
+ stroke(27)
+ strokeWeight(2)
+ ellipse(x,y,r1*20,r1*12)
+ fill(145,20,129)
+ ellipse(x+r1*5,y,r1*5,r1*2)
+ fill(145,20,129)
+ ellipse(x-r1*5,y,r1*5,r1*2)
+ fill(162,147,150)
+//   ellipse(x-r1*2,y,r1*4,r1*2)
+ 
+              }
+            }
+          }
+            
+          
+             // function draw(){
+//let spacing = 10
+//for (var x =0; x<width; x+= spacing){
+
+  //for (var y =0; y<height; y+= spacing){
+
+
+  // if (millis() >= 300+timer) {
+
+  //flower(x,y,[random(255),random(255),random(255)],random(2,12))
+   
+   // timer = millis();
+//
+//}
+//}
+//
+  
