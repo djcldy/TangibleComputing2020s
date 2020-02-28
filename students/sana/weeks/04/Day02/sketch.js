83dbaf58cad940ac0d39db329fbb21e7e3e2fddf
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-02-28 18:06:06 +0400

    Update sketch.js

--- students/sana/weeks/04/Day02/sketch.js
+++ students/sana/weeks/04/Day02/sketch.js
@@ -1,12 +1,11 @@
+let a = 0
 
-
-let a = 0.0;
-let s = 0.0;
+let s = 0
 
 
 function setup() {
   createCanvas(800, 800);
-  //background (255)
+ // background (255)
             
          //     let spacing = 100
 //for (var x =0; x<width; x+= spacing){
@@ -23,7 +22,7 @@ function setup() {
 function cat(x,y,color,r1) {
 
 //face
- fill(color)
+ fill(255)
  stroke(27)
  strokeWeight(2)
  ellipse(x,y,r1*18,r1*13)
@@ -74,18 +73,18 @@ arc(x+50,y-105,r1*2,100,0,4)
  
               }
       function draw() {
-  background(102);
+     
+  background([random(255),random(255),random(255)],random(1,20))
 
-  //Slowly increase 'a' and then animate 's' with
-  //a smooth cyclical motion by finding the cosine of 'a'
-  a = a + 0.04;
-  s = cos(a) * 2;                 
+
+  a = a + .01;
+  s = cos(a) * 1;                 
           
       translate(width / 2, height / 2);
   scale(s);
-  fill(color);
+  fill(133);
 
-cat(0,0,50,50)
+cat(10,11,18,13);
 }
 
   
