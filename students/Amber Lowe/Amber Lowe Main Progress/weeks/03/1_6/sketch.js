5a53a81b56a4efc43c41b58cdd8031057a114bda
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-02-29 08:10:53 +0400

    updates

--- students/Amber Lowe/Amber Lowe Main Progress/weeks/03/1_6/sketch.js
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/03/1_6/sketch.js
@@ -1,8 +1,11 @@
-let capture;
+// SP: Added comments 
+
+let capture
 let res = 10
 let res2 = 30
 
 function setup() {
+
   createCanvas(800, 400);
   capture = createCapture(VIDEO);
   capture.size(800, 400);
@@ -11,24 +14,28 @@ function setup() {
   stroke(255)
   image(capture,0,0)
   // noStroke()/
+
 }
 function draw(){
 
-	diagonalarray()
+
+  diagonalArray() // SP: write function names using camelCase, it still works but this is convention
+	// diagonalarray()
 
 }
 
 
-function diagonalarray() { //mirror elements with girds, layered and sharp movement 
+function diagonalArray() { 
 
+  // mirror elements with girds, layered and sharp movement
   // background(255);
-
+  let s = second() // call this function here. you only need to get the time once. in the four loop it retrieves time multiple times
+  
   for (var x = 0; x < width; x += res) {
 
-    let s = second()
+    // let s = second() //
     let diff = Math.abs(s/60 - x/height)
     let riff = Math.abs(s/58 - y*width)
-
 
     if (diff > 0.1) continue
     if (riff < 0.1) continue	
@@ -36,9 +43,7 @@ function diagonalarray() { //mirror elements with girds, layered and sharp movem
     for (var y = sin(20); y < width; y += res2){
 
         let c = capture.get(x,y/2) // gets pixel color of canvas returns [r,g,b,a]
-
         rotate(x/25)
-    
         fill(c[1],0,c[2])
         rect(x,y,res,res)
         triangle(x,y,res2,res,res,res2)
@@ -47,16 +52,8 @@ function diagonalarray() { //mirror elements with girds, layered and sharp movem
         stroke(c[1],0,c[2])
         rect(x,y,res,res)
 
-
-    }
-
-
-   
+      }
 	
+    }
 }
-
-
-
-
-  }
 
