3eb28c29679dfa7e9e15618c9fe782d82828a8a2
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-02-29 08:36:18 +0400

    updates sana - suggestion

--- students/sana/weeks/04/Day04/sketch.js
+++ students/sana/weeks/04/Day04/sketch.js
@@ -2,9 +2,25 @@
 
 function setup(){
 
-  createCanvas(800, 800);
+  createCanvas(800, 800)
+  rectMode(CENTER) // draws rectangle at center
 
 
+}
+
+function draw() {
+
+  background(0) 
+  // let second=60 // don't need this 
+  push()
+  translate(width/2, height/2);
+  moonLight(sin(frameCount/100)*width/4,sin(frameCount/20)*height/8,second()/4) // SP: another way
+  // moonLight(frameCount/second*20,13,12,49,20+(frameCount/4))
+  pop()
+  
+
+             
+         
 }
 
 
@@ -12,32 +28,21 @@ function moonLight(x,y,r1){
 
   
   translate(x,y)
-
-fill(245)
-  ellipse(x,y,r1*30,r1*30);
+  rotate(frameCount/100) // SP Added some rotation
+  fill(245)
+  rect(x,y,r1*30,r1*30);
   fill(255,9,30)
-  ellipse(x,y-2,r1*20,r1/2);
-   fill(164,69,130)
-    ellipse(x,y+6,r1*60,r1/2);
- ellipse(x,y-2,r1*20,r1/2);
-   fill(64,9,110)
-    ellipse(x-80,y+40,r1*60,r1/2);
-fill(64,9,110)
-    ellipse(x-80,y+40,r1*60,r1/2);
-    fill(69,90,110)
-    ellipse(x-150,y+100,r1*60,r1);
-  }
-function draw() {
-      background(0) 
-  let second=60
-  
-  push();
-  translate(width/2, height/2);
-  moonLight(frameCount/second*20,13,12,49,20+(frameCount/4));
-  pop();
-  
+  rect(x,y-2,r1*20,r1/2);
+  fill(164,69,130)
+  rect(x,y+6,r1*60,r1/2);
+  rect(x,y-2,r1*20,r1/2);
+  fill(64,9,110)
+  rect(x-80,y+40,r1*60,r1/2);
+  fill(64,9,110)
+  rect(x-80,y+40,r1*60,r1/2);
+  fill(69,90,110)
+  rect(x-150,y+100,r1*60,r1);
 
-             
-         
 }
+
 
