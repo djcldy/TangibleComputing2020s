81716a134ff378baf1b4be6d77ac9547aa4a7361
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-03-01 16:46:46 +0400

    update

--- students/sana/weeks/05/Day01/sketch.js
+++ students/sana/weeks/05/Day01/sketch.js
@@ -1,49 +1,18 @@
 
- 
-let timer = 0;
-function setup(){
-              createCanvas(800,800)
-         
-}
 
+  let list = ['honey','flour','milk','chocolate','cereal']
+  let verbs = ['sitting','laughing','kissing','running','seeing']
+  let names = ['phil','amber','sana','abd','moh']
+  let numElements= list*length
+  console.log(list[0])
+  console.log(list[3])
+  console.log(numElements)
 
-          function flower(x,y,color,r1){
+  for (var i = 0; i < numElements; i++){
+    console.log(list[i])
 
-
-
-fill(145,20,129)
- ellipse(x+r1*2,y,r1*6,r1*4)
- fill(69,112,55)
- ellipse(x-r1*2,y,r1*6,r1*4)
- fill(80,113,241)
- ellipse(x,y-r1*2,r1*6,r1*3,r1*4)
- fill(187,33,166)
- ellipse(x,y+r1*2,r1*6,r1*4,r1*5)
- fill(85,40,33)
- ellipse(x,y,r1*4,r1*6,r1*2)
-  fill(85,17,220)
-  ellipse(x+r1*2,y,r1*4,r1*2)
-  fill(162,147,150)
-  ellipse(x-r1*2,y,r1*4,r1*2)
- fill(color)
- ellipse(x,y,r1*2,r1*2)
- 
-              }
-              function draw(){
-let spacing = 10
-for (var x =0; x<width; x+= spacing){
-
-  for (var y =0; y<height; y+= spacing){
-
-
-   if (millis() >= 300+timer) {
-
-  flower(x,y,[random(255),random(255),random(255)],random(2,12))
-   
-    timer = millis();
+    let msg = 'my'+ list[i]
+    console.log(msg)
   }
 
-}
-}
-}
 
