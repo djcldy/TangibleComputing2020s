f05f33616ede53ef60f421d3579b85ced17ff925
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-03-04 14:12:08 +0400

    Update sketch.js

--- students/sana/weeks/05/Day02/sketch.js
+++ students/sana/weeks/05/Day02/sketch.js
@@ -1,90 +1,38 @@
-let a = 0
+let list = ['😊','♥️','🌚','🦄','😊']
+  let verbs = ['♥️','🦄','🦄','😊','🌚']
+  let names = ['🌚','😊','🦄','🦄','♥️']
+  let fonts = [ 'Times New Roman', 'Impact','Comic Sans']
 
-let s = 0
+  let numElements= list.length
+  console.log(list[0])
+  console.log(list[3])
+  console.log(numElements)
 
+  function setup(){
+    createCanvas(500,500)
+    background(0)
+    let spacing  = height/numElements
+    textAlign(CENTER)
 
-function setup() {
-  createCanvas(800, 800);
- // background (255)
-            
-         //     let spacing = 100
-//for (var x =0; x<width; x+= spacing){
+  for (var i = 0; i < numElements; i++){
+    //console.log(list[i])
 
-// for (var y =0; y<height; y+= spacing){
-//cat(x,y,[random(255),random(255),random(255)],random(1,20))
-     
-   
-}
-//}
-  
-//}
+    //let msg = 'my'+ list[i]
+    let msg = names[i]+"'s " + "and " + random(names)+ "'is "+ verbs[i]
+    console.log(msg)
 
-function cat(x,y,color,r1) {
+    let numchar = msg.length
+    let size =width/numchar
+    textFont(random(fonts))
+    //console.log(size)
+    textSize(size*2)
+    for (var j = 0; j < numchar;j++){
+      fill(255,50)
+       text(msg[j],size*j+size,spacing*i+size)
+    fill(random(255),random(255),random(255))
+    text(msg[j],size*j,spacing*i)
 
-//face
- fill(255)
- stroke(27)
- strokeWeight(2)
- ellipse(x,y,r1*18,r1*13)
- //eyes
- fill(255)
- ellipse(x+r1*5,y,r1*4,r1)
- fill(255)
- ellipse(x-r1*5,y,r1*4,r1)
- //eye pupil
- fill(34,175,150)
-ellipse(x-r1*5,y,r1,r1)
- fill(34,175,150)
-ellipse(x+r1*5,y,r1,r1)
- fill(255)
-ellipse(x-r1*5,y,r1/2,r1)
- fill(255)
-ellipse(x+r1*5,y,r1/2,r1)
-//nose
- fill(0)
- stroke(0)
-ellipse(x,y+18,r1*2,8)
-//wiskes
-stroke(39)
-noFill()
-arc(x-26,y+16,r1*2,20,0,3)
-stroke(39)
-noFill()
-arc(x-26,y+24,r1*2,20,0,3)
-stroke(39)
-noFill()
-arc(x+26,y+16,r1*2,20,0,3)
-stroke(39)
-noFill()
-arc(x+26,y+24,r1*2,20,0,3)
-
-//mouth
-fill(37,50,99)
-stroke(22,59,20)
-arc(x,y+40,r1*2,-24,1,-4,-6)
-// ears
-fill(10)
-stroke(0)
-arc(x-50,y-105,r1*2,100,0,4)
-fill(10)
-stroke(0)
-arc(x+50,y-105,r1*2,100,0,4)
-
- 
-              }
-      function draw() {
-     
-  background([random(255),random(255),random(255)],random(1,20))
-
-
-  a = a + .01;
-  s = cos(a) * 1;                 
-          
-      translate(width / 2, height / 2);
-  scale(s);
-  fill(133);
-
-cat(10,11,18,13);
+  }
 }
 
-  
+}
