e062bfa260a4f2761adb61aa0c10204014523449
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-03-06 11:49:46 +0400

    Update Week 4
    Text generator

--- students/Amber Lowe/Amber Lowe Main Progress/weeks/04/1_7/sketch.js
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/04/1_7/sketch.js
@@ -1,44 +1,91 @@
 
 
 let onesyllable = ['life','love','day','one','near','man']
-let twosyllable = ['nature','bread','future','death','heaven','dance']
+let twosyllable = ['nature','loose','future','death','heaven','dance']
 let threesyllable = ['amazement','banana','dilemma','eletric','curious','bicycle']
 let fonts = ['Arial', 'Georgia', 'Times New Roman', 'Verdana', 'Courier', 'Impact','Comic Sans']
 
 
-let numElements = 4 //
-console.log(onesyllable[0]) // prints honey
-console.log(threesyllable[3]) // prints chocolate
+let numElements = 6
+console.log(onesyllable[0]) 
+console.log(threesyllable[3])
 console.log(numElements)
 
 
 function setup(){
 
-	createCanvas(1200,500)
-	background(13,5,70)
-	let spacing = height/numElements 
-
-
-for (var i = 0; i < numElements; i++){
-
-    let msg = random(onesyllable)  +  random(twosyllable)  + ' is ' +  twosyllable[i]  +
-              random(threesyllable)  +  onesyllable[i]  + ' for ' +  twosyllable[i]  +
-	          random(onesyllable)  +  random(threesyllable)  + ' to ' +  onesyllable[i] 
 	
-	console.log(msg)
+  createCanvas(1600,800)
+  textAlign(CENTER,CENTER)
+  
+  generateMeme()
 
-    let numChar = msg.length
-    let size = width/numChar
-    //console.log(size)
-    textFont(random(fonts))
-    textSize(size*2)
-
-    
-    for(var j = 0; j < numChar; j++ ){
-	
-	fill(255,50)
-	fill(random(255),random(255),random(255))
-	text(msg,5,spacing*j)
-   }
 }
+
+
+
+function generateMeme(){
+
+  let backgroundColor = getRandomColor()
+  background(backgroundColor)
+
+  let numLines = int(random())
+  
+  let fontColor = getRandomColor()
+  fill(fontColor)
+ 
+  let message = getMessage()
+  let message2 = getMessage()
+ 
+  let font = random(fonts)
+  textFont(font)
+ 
+  let size = width/message.length
+  textSize(size)
+
+  for (var i = 0; i < message.length; i++){
+
+    // fill(255)
+    // text(message[i],size*i,height/2)
+
+    for (var j = 1; j < 2; j+=.1){
+
+      push()
+      translate(size*i,height/2)
+      scale(j)
+      fill(fontColor[0],fontColor[1],fontColor[2],50)
+      text(message[i],0,0)
+      scale(j*2)
+      text(message2[i],10,10)
+
+
+      pop()
+
+    }
+
+
+  }
+
+}
+
+
+function getMessage(){
+
+  let name = random(onesyllable)
+  let adjective = random(threesyllable)
+  let noun = random(twosyllable)
+  let verb = random(onesyllable)
+  let message = ' Okay ' + random(onesyllable)  + ' ' + random(twosyllable) + ' is ' + random(threesyllable) + ' '
+  let message2 = ' Okay ' + name  + 'from ' + adjective + ' to ' + verb 
+  
+  return message
+
+}
+
+function getRandomColor(){
+
+  // returns randomColor
+  let color = [random(255),random(255),random(255)]
+  return color 
+
 }
