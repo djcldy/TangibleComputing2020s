f22a3417e85b54d7bf87bc21c969f6052c655491
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-03-17 12:38:53 +0400

    Update sketch.js

--- students/sana/weeks/05/Day02/sketch.js
+++ students/sana/weeks/05/Day02/sketch.js
@@ -1,23 +1,89 @@
-// let emojis = ['😋','😍','😉','🙃','😘','😭','😜','😝','😛','😎','🤓','😨','😱','😳','😰','😥','😓','🤗','🤥','🙄','😧','😲','😴','🤤','😪','😵','🤐','🤢','🤒','🤖']            
-//   console.log(emojis)
+let enemies = []
 
-//   function setup(){
-//     createCanvas(500,500);
-//   }
-//     function draw(){
- 
-//     let spacing  = height/emojis
-    
 
-//   for (var i = 0; i < emojis; i++){
+function setup(){
 
+  createCanvas(500,500)
+
+  for (var i = 0; i < 50; i++){
+
+    enemies.push(createEnemy())
+
+
+  }
+
+  fill(0)
+
+}
+var score = 0;
+var player = {
+	color : "#FFF",
+	x : 280,
+	width : 40,
+	y : 355,
+	height: 40,
+	draw : function(){
+		image(img_player, this.x, this.y, this.width, this.height);
+    },
+}
+
+
+function draw(){
+
+  background(255)
+  fill(0)
+
+  for (var i =0; i < enemies.length; i++){
+
+    let enemy = enemies[i]
+
+    enemy.x += enemy.vx
+    enemy.y += enemy.vy 
+
+    if (enemy.x > width) enemy.x = 0 
+    if (enemy.x < 0) enemy.x = width 
+    if (enemy.y > height) enemy.x = 0 
+    if (enemy.x < 0) enemy.x = height
+
+    if (dist(mouseX,mouseY,enemy.x,enemy.y) < 100 ){
+
+      enemy.vx = (mouseX - enemy.x)/width 
+      enemy.vy = (mouseY - enemy.y)/height
   
-//     let msg = emojis[i]+" ♥️" +  random(emojis)
-   
 
- 
-    
-// }
-//   }
+    }
 
+
+    ellipse(enemy.x,enemy.y,5,5)
+
+
+
+  }
+
+  drawMouse()
+
+
+
+}
+
+function drawMouse(){
+
+  fill(255,0,0,50)
+  ellipse(mouseX,mouseY,100,100)
+
+}
+
+function createEnemy(){
+
+  // do tutorial 'object' we can use this to make swarms 
+  let enemy = {}
+  enemy.x =  random(width)
+  enemy.y =  random(height)
+  enemy.vx = random(-1,1)
+  enemy.vy = random(-1,1)
+
+
+  return enemy 
+
+}
 
