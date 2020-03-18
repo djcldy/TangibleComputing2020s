8f2364821606df660633561d5e2c033ea2a5e270
sanoya-18 <60841651+sanoya-18@users.noreply.github.com>
2020-03-18 23:17:11 +0400

    Update sketch.js

--- students/sana/weeks/05/Day02/sketch.js
+++ students/sana/weeks/05/Day02/sketch.js
@@ -1,4 +1,6 @@
-//this is mario 
+
+
+
 function setup() {
   
   createCanvas(800, 800);
@@ -15,7 +17,9 @@ function draw(){
 }
   
   function mario(){
- 
+ translate(mouseX,mouseY);
+  
+  
   fill(245,0,0)
   rect(0,0,220,45)
   fill(255,0,0)
@@ -74,10 +78,89 @@ function draw(){
   rect(-50,380,200,80)
   fill(0)
   rect(210,285,90,50)
+
+  
+}
+
+// function setup() {
+  
+//   createCanvas(800, 800);
+//   rectMode(CENTER)
+//   noStroke()
+
+// }
+
+
+// function draw(){
+
+//   background(random(255),0,random(255));
+//   mario();
+// }
+  
+//   function mario(){
+ 
+//   fill(245,0,0)
+//   rect(0,0,220,45)
+//   fill(255,0,0)
+//   rect(0,10,250,55)
+//   fill(255,0,44)
+//   rect(0,20,350,25)
+//   fill(255,50,0)
+//   rect(0,55,400,50)
+//  fill(0)
+//   square(-300,350,200,300,10,10,45)
+//   fill(0)
+//   square(-320,200,280,700,10,0,115)
+//   fill(255,10,0)
+//   rect(0,60,450,50)
+//    fill(255,0,0)
+//   rect(35,70,600,55)
+//    fill(0)
+//   rect(-60,120,700,50)
+//   fill(0)
+//   rect(-60,200,700,200)
+//   fill(160)
+//   rect(-10,150,750,40)
+//   fill(177)
+//   rect(100,200,600,60)
+//   fill(190)
+//   rect(-100,240,700,80)
+//   fill(0)
+//   rect(-20,290,650,50)
+//   fill(0)
+//   rect(-20,300,750,100)
+//   fill(189)
+//   rect(50,280,400,90)
+//   fill(180)
+//   rect(55,340,600,60)
+//   fill(180)
+//   rect(-50,360,700,50)
+//  fill(0)
+//   rect(-50,385,700,50)
+//   fill(0)
+//   rect(0,440,400,70)
+//   fill(0)
+//   rect(0,500,400,70)
+//   fill(0)
+//   square(100,180,100,400,10)
+//   fill(0)
+//   square(-150,170,200,300,10,10)
+//   fill(0)
+//   square(-150,350,200,300,10,10,45)
+//   fill(0)
+//   rect(-50,450,300,80)
+//   fill(0)
+//   rect(-50,150,100,50)
+//   fill(0)
+//   rect(-50,280,200,50)
+//   fill(0)
+//   rect(-50,380,200,80)
+//   fill(0)
+//   rect(210,285,90,50)
  
   
 
-}
+// }
 //here the object i managed to write
 //
 // var player = {x:0,
