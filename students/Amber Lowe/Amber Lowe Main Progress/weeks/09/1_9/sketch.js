a79a894dbfa391def2d0881a0795ab4742ad1b62
AmberJaneLowe <60570029+AmberJaneLowe@users.noreply.github.com>
2020-04-06 10:44:05 +0400

    Update Sketch Week 9 Day 9

--- students/Amber Lowe/Amber Lowe Main Progress/weeks/09/1_9/sketch.js
+++ students/Amber Lowe/Amber Lowe Main Progress/weeks/09/1_9/sketch.js
@@ -1,15 +1,18 @@
 let i = 0 
-let capW = 500 // width capture 
-let capH = 500 // height capture
-let cellW = 40 // width cell 
-let cellH = 40  // height cell 50
+let capW = 800 // width capture 
+let capH = 800 // height capture
+let cellW = 100 // width cell 
+let cellH = 100  // height cell 50
 let cells = []
+
+let vid 
 
 function setup(){
 	
 	
-	createCanvas(500,500,WEBGL)
-	
+	createCanvas( 800, 800, WEBGL)
+	vid = createCapture( 800, 800) 
+	vid.hide()
 	id = 0
 	for (let x = 0; x < width; x += cellW) {
 		
@@ -35,14 +38,15 @@ function draw(){
 	
 	background(255)
 
-    ambientLight(500)
+    ambientLight(300)
     directionalLight(255, 255, 255, 0.25, 0.25, 0)
     pointLight(255,0, 255, width,height, 250)
 
     let offset = sin(frameCount/100)*200 
     let scl = (sin(frameCount/100)+2)*0.5
     stroke(255)
-	
+	push()
+	scale(0.5)
 	for (i = 0; i < cells.length; i++){
 		
 		let cell = cells[i] 
@@ -50,6 +54,7 @@ function draw(){
 		cell.display()
 		
 	}
+	pop()
 	
 	if (random()>0.99){
 			swapPosition( cells[second()], cells[int(random(cells.length))])
@@ -80,16 +85,19 @@ function Cell(x,y,tx,ty,w,h,col,id){
 	this.id = id 
 	this.x = x 
 	this.y = y 
+	this.px = x 
+	this.py = y 
 	this.tx = tx 
 	this.ty = ty 
 	this.tr = 0 
 	this.r = 0 
 	this.w = w 
 	this.h = h 
-    this.col = col
+	this.col = col
 	
 	this.update = function(){
 		
+		this.col = vid.get( this.px, this.py )
 		this.r = (this.tr - this.r)*0.1 + this.r
 		this.x = (this.tx - this.x)*0.1 + this.x
 		this.y = (this.ty - this.y)*0.1 + this.y
@@ -104,13 +112,18 @@ function Cell(x,y,tx,ty,w,h,col,id){
 		
 		translate(this.x-width/2,this.y-height/2)
 		rotateZ(this.r)
+		//col = this.col.push(10)
 		
 		fill(this.col) 
+		
+		//for (j = 0; j < 50; j+10){
 		box(this.w,this.h,this.w)
-	
+				
+		}
+
 		
 		pop()
 	}
 	
 	
-}
+//}
