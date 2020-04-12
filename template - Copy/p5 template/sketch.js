f82e622a21ce44097bc6224d1cf0974abcd15fbb
Sayjel Patel <sayjel.v.patel@gmail.com>
2020-04-12 14:54:58 +0400

    update

--- template - Copy/p5 template/sketch.js
+++ template - Copy/p5 template/sketch.js
@@ -1,47 +1,85 @@
+// Example Code
+// TRY Incorperating the following variables in your code 
+// leftHandX,leftHandY,rightHandX,rightHandY,leftEyeX,leftEyeY,rightEyeX,rightEyeY
+
+function setup() {
+    createCanvas(640, 480);
+    loadPoseNet()
+    background(255);
+}
+
+function draw() {
+    background(vid.get())
+    fill(255,0,255)
+    ellipse(noseX, noseY, 20, 20);
+    ellipse(rightHandX, rightHandY, 20, 20);
+    ellipse(leftHandX, leftHandY, 20, 20);
+    ellipse(leftEyeX, leftEyeY, 20, 20);
+    ellipse(rightEyeX, rightEyeY, 20, 20);
+}
+
+// dont touch this code here 
+
+// poseNet Variables
+let vid
+let noseX = noseY = rightHandX = rightHandY = leftHandX = leftHandY = leftEyeX = leftEyeY = rightEyeX = rightEyeY = 0
+let img;
+let poseNet;
+let poses = [];
 
 
-function setup(){
+function loadPoseNet() {
 
-	createCanvas(500,500)
+    vid = createCapture(640, 480)
+        // vid.hide()
+    poseNet = ml5.poseNet(vid, modelLoaded);
+
+    // set some options
+    let options = {
+        imageScaleFactor: 1,
+        minConfidence: 0.25
+    }
+
+    // Listen to new 'pose' events
+    poseNet.on("pose", function(results) {
+
+        poses = results;
+        if (!poses[0]) return
+        let pose = poses[0].pose
+
+        if (pose) {
+
+            noseX = updateValue('nose', 'x', pose, noseX)
+            noseY = updateValue('nose', 'y', pose, noseY)
+            rightHandX = updateValue('rightWrist', 'x', pose, rightHandX)
+            rightHandY = updateValue('rightWrist', 'y', pose, rightHandY)
+            leftHandX = updateValue('leftWrist', 'x', pose, leftHandX)
+            leftHandY = updateValue('leftWrist', 'y', pose, leftHandY)
+            leftEyeX = updateValue('leftEye', 'x', pose, leftEyeX)
+            leftEyeY = updateValue('leftEye', 'y', pose, leftEyeY)
+            rightEyeX = updateValue('rightEye', 'x', pose, rightEyeX)
+            rightEyeY = updateValue('rightEye', 'y', pose, rightEyeY)
+
+        }
+
+
+    });
 
 }
 
-function draw(){
-
-	background(255)
-
-	
-	let txt = getInputValue()
-
-	if (txt.includes('rana') ){
-
-		background(255,0,0)
-
-	}
-
-	if (txt.includes('circle')){
-
-		fill(255,0,255)
-
-		circle(100,100,100)
+function updateValue(key1, key2, kp, curr) {
 
 
-	}
+    if (kp[key1].confidence > 0.2) {
+        return (kp[key1][key2]-curr)*0.5+curr
+    }
 
-	textSize(12)
-	text(txt,50,50)
 
 
+    return curr
 
 }
 
-
-
-
-function getInputValue(){
-
-	// Selecting the input element and get its value 
-	return inputVal = document.getElementById("fname").value
-
-
+function modelLoaded() {
+    console.log('model is loaded')
 }
